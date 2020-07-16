112e27c0ac48a1c6984a65abfc1667956f261fcf
chainsawriot <chainsawtiney@gmail.com>
2020-07-16 20:33:17 +0200

    Seeking for RProj in package base dir

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 
 RStudio, we can be friend, right?
 
-This elisp project seeks for .Rproj file in the current directory and set indentation of ESS according to the .Rproj file. Useful for ESS users surviving in this RStudio-dominant world.
+This elisp project seeks for .Rproj file in the current directory or the base directory of your current developing R package's directory. And then set indentation of ESS according to the .Rproj file. Useful for ESS users surviving in this RStudio-dominant world.
 
 TODO
 
-- [ ] Finding .Rproj file in the base directory of a package (if it is a package).
+- [X] Finding .Rproj file in the base directory of a package (if it is a package).
--- ess_rproj.el
+++ ess_rproj.el
@@ -1,21 +1,30 @@
 
-(defun readproj ()
+(defun readproj (rproj)
   (with-temp-buffer
-    (insert-file-contents (car (directory-files (expand-file-name default-directory) t "\\.[Rr]proj$")))
+    (insert-file-contents rproj)
     (split-string (buffer-string) "\n" t)
     )
   )
 
-(defun set_ess_indent_proj ()
+(defun set_ess_indent_proj (rproj)
   (setq-local ess-indent-level
 	      (string-to-number (nth 1 (seq-find (lambda (x) (string= (car x) "NumSpacesForTab"))
-						 (mapcar #'(lambda (x) (split-string x ": ")) (readproj))))))
+						 (mapcar #'(lambda (x) (split-string x ": ")) (readproj rproj))))))
   (message "R Project file found. Set indentation to: %s." ess-indent-level)
   )
 
+;; search for .Rproj first in the default-directory, if it can't determine if it is an R package. Search for .Rproj in ess-r-package--find-package-path. Return nil otherwise
+
+
+(defun seek_rproj (directory)
+  (car (directory-files (expand-file-name directory) t "\\.[Rr]proj$"))
+  ) 
+
 (defun ess_rproj ()
   (interactive)
-  (if (directory-files (expand-file-name default-directory) t "\\.[Rr]proj$")
-      (set_ess_indent_proj)
-    (message "R Project file not found."))
+  (cond ((seek_rproj default-directory) (set_ess_indent_proj (seek_rproj default-directory)))
+	((seek_rproj (ess-r-package--find-package-path)) (set_ess_indent_proj (seek_rproj (ess-r-package--find-package-path))))
+
+    (t (message "R Project file not found.")))
   )
+
