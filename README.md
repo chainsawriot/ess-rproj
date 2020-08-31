7518382c9a65389c23bcd4a525de70e1e5f2ed42
chainsawriot <chainsawtiney@gmail.com>
2020-08-31 23:45:56 +0200

    Make the elisp more elisp-like. Update README.md

--- README.md
+++ README.md
@@ -4,8 +4,13 @@
 
 RStudio, we can be friend, right?
 
-This elisp project seeks for .Rproj file in the current directory or the base directory of your current developing R package's directory. And then set indentation of ESS according to the .Rproj file. Useful for ESS users surviving in this RStudio-dominant world.
+This elisp project seeks for .Rproj file in the current directory or the base directory of your current developing R package. And then set indentation of ESS according to the .Rproj file. Useful for ESS users surviving in this RStudio-dominant world.
 
-TODO
+## Installation
 
-- [X] Finding .Rproj file in the base directory of a package (if it is a package).
+Download `ess_rproj.el` somewhere. Put this into your configuration file.
+
+```lisp
+(load-file "ess_rproj.el")
+(add-hook 'ess-mode-hook #'ess-rproj)
+```
--- ess_rproj.el
+++ ess_rproj.el
@@ -1,41 +1,39 @@
 
-(defun readproj (rproj)
+(defun read-proj (rproj)
   (with-temp-buffer
     (insert-file-contents rproj)
     (split-string (buffer-string) "\n" t)
     )
   )
 
-(defun set_ess_indent_proj (rproj)
+(defun set-ess-indent-proj (rproj)
   (if (null rproj)
       (message "R Project file not found.")
     (progn
       (setq-local ess-indent-level
 		  (string-to-number (nth 1 (seq-find (lambda (x) (string= (car x) "NumSpacesForTab"))
-						     (mapcar #'(lambda (x) (split-string x ": ")) (readproj rproj))))))
+						     (mapcar #'(lambda (x) (split-string x ": ")) (read-proj rproj))))))
   (message "R Project file found. Set indentation to: %s." ess-indent-level)
       )
     )
   )
 
-;; search for .Rproj first in the default-directory, if it can't determine if it is an R package. Search for .Rproj in ess-r-package--find-package-path. Return nil otherwise
 
-
-(defun seek_rproj (directory)
+(defun seek-rproj (directory)
   (car (directory-files (expand-file-name directory) t "\\.[Rr]proj$"))
   ) 
 
 
-(defun getrproj ()
+(defun get-rproj ()
   "if default directory is an R package, return full path to the root directory; otherwise, return full path of default directory"
   (setq-local root (plist-get (ess-r-package-info default-directory) :root))
-    (cond ((null root) (seek_rproj (expand-file-name default-directory)))
-	  ((stringp root) (seek_rproj (expand-file-name root)))
+    (cond ((null root) (seek-rproj (expand-file-name default-directory)))
+	  ((stringp root) (seek-rproj (expand-file-name root)))
     ))
 
 
-(defun ess_rproj ()
+(defun ess-rproj ()
   (interactive)
-  (setq-local x (getrproj))
-  (set_ess_indent_proj x))
+  (set-ess-indent-proj (get-rproj))
+  )
 
