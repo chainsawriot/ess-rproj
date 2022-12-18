9f54131e63b48471363552b10aa248f384cf3649
chainsawriot <chainsawtiney@gmail.com>
2022-12-18 19:04:53 +0100

    Convert to a minor mode

--- ess-rproj.el
+++ ess-rproj.el
@@ -1,3 +1,4 @@
+(require 'ess)
 
 (defun read-proj (rproj)
   (with-temp-buffer
@@ -6,34 +7,39 @@
     )
   )
 
-(defun set-ess-indent-proj (rproj)
+
+(defun seek-rproj (directory)
+  (car (directory-files (expand-file-name directory) t "\\.[Rr]proj$"))
+  )
+
+
+(defun get-rproj ()
+  "if default directory is an R package, return full path to the root directory; otherwise, return full path of default directory"
+  (setq-local root (plist-get (ess-r-package-info default-directory) :root))
+  (cond ((null root) (seek-rproj (expand-file-name default-directory)))
+	((stringp root) (seek-rproj (expand-file-name root)))
+	))
+
+(defun set-ess-indent-rproj ()
+  (interactive)
+  (setq rproj (get-rproj))
   (if (null rproj)
       (message "R Project file not found.")
     (progn
       (setq-local ess-indent-level
 		  (string-to-number (nth 1 (seq-find (lambda (x) (string= (car x) "NumSpacesForTab"))
 						     (mapcar #'(lambda (x) (split-string x ": ")) (read-proj rproj))))))
-  (message "R Project file found. Set indentation to: %s." ess-indent-level)
+      (message "R Project file found. Set indentation to: %s." ess-indent-level)
       )
     )
   )
 
-
-(defun seek-rproj (directory)
-  (car (directory-files (expand-file-name directory) t "\\.[Rr]proj$"))
-  ) 
-
-
-(defun get-rproj ()
-  "if default directory is an R package, return full path to the root directory; otherwise, return full path of default directory"
-  (setq-local root (plist-get (ess-r-package-info default-directory) :root))
-    (cond ((null root) (seek-rproj (expand-file-name default-directory)))
-	  ((stringp root) (seek-rproj (expand-file-name root)))
-    ))
-
-
-(defun ess-rproj ()
-  (interactive)
-  (set-ess-indent-proj (get-rproj))
+;;;###autoload
+(define-minor-mode ess-rproj
+  "TBA"
+  :lighter " rproj"
   )
 
+(add-hook 'ess-mode-hook #'set-ess-indent-rproj)
+
+(provide 'ess-rproj)
